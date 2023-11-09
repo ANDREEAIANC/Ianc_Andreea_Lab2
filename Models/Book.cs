2e443bbcd30da0bd323da68a24f348e92019753d
Andreea Ianc <andreea.ianc08@yahoo.com>
2023-11-09 18:33:37 +0200

    Laborator5

--- Models/Book.cs
+++ Models/Book.cs
@@ -23,6 +23,8 @@ namespace Ianc_Andreea_Lab2.Models
 
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; }
+
+   
         public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
---
+++ Pages/Members/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ianc_Andreea_Lab2.Data;
+using Ianc_Andreea_Lab2.Models;
+
+namespace Ianc_Andreea_Lab2.Pages.Members
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ianc_Andreea_Lab2.Data.Ianc_Andreea_Lab2Context _context;
+
+        public IndexModel(Ianc_Andreea_Lab2.Data.Ianc_Andreea_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Member> Member { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Member != null)
+            {
+                Member = await _context.Member.ToListAsync();
+            }
+        }
+    }
+}
